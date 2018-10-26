849c52a17ffe3450b236f6f1e145c8c972b33c99
Leon Foks <nfoks@usgs.gov>
2018-10-26 05:58:55 -0600

    minor

--- src/core/m_parameters.f90
+++ src/core/m_parameters.f90
@@ -2,7 +2,8 @@ module m_parameters
   !! Module contains user defined parameters for consistent use
 use variableKind
 implicit none
-real(r64), parameter :: inf = huge(0.d0)
+real(r64), parameter :: inf = huge(0.0_r64)
 real(r64),parameter :: NaN = transfer((/ Z'00000000', Z'7FF80000' /),1.0_r64)
-real(r64),parameter :: pi = dacos(-1.d0)
+real(r64),parameter :: pi = dacos(-1.0_r64)
+real(r64), parameter :: zero = 0.0_r64
 end module
